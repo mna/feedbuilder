3046d1243caadd82fc37516e448bf0ab3a572dd9
Martin Angers <martin.n.angers@gmail.com>
2011-07-28 20:47:47 -0400

    FB MVP complete

--- public/js/feedbuilder.v1.js
+++ public/js/feedbuilder.v1.js
@@ -1,5 +1,6 @@
 $(function(){
     $('a[data-item-id]').live('click', function() {
         $('#hidId').val($(this).attr('data-item-id'));
+        $('#theForm').filter(':enabled').submit(); 
     });
 });
